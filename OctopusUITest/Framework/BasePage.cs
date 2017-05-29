5631563630c5461f06369359fc6afa98fbeb618d
Sandesh Prasanna <sandeshp7@gmail.com>
2017-05-29 14:39:14 +0100

    Changes to BasePage.cs. Formatting changes

--- OctopusUITest/Framework/BasePage.cs
+++ OctopusUITest/Framework/BasePage.cs
@@ -12,7 +12,7 @@
 	        private IWebDriver driver;
 
 			//Getter method for the webDriver
-			public IWebDriver getDriver()
+			public IWebDriver GetDriver()
 			{
 				return this.driver;
 			}
@@ -22,24 +22,23 @@
 		     * The browser will be launched for each test. 
 		     */
 			[SetUp]
-			public void beforeMethod()
-				{
-	                // Runs on Chrome.
-	                //Specify the path of the chrome driver.
-	                driver = new ChromeDriver("/Users/Sandesh/Desktop"); 
-	                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
-					driver.Manage().Window.Maximize();
-	                driver.Navigate().GoToUrl("https://www.octopusinvestments.com/adviser/about-us/our-people");
-				}
+			public void BeforeMethod()
+			{
+                // Runs on Chrome.
+                //Specify the path of the chrome driver.
+                driver = new ChromeDriver("/Users/Sandesh/Desktop"); 
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://www.octopusinvestments.com/adviser/about-us/our-people");
+			}
 
 			/*
 		     * This method will always be executed after every test method.
 		     * The browser will be closed after each test. 
 		     */
 			[TearDown]
-			public void afterMethod()
-				{
-		            driver.Quit();
-				}
+			public void AfterMethod()
+			{
+	            driver.Quit();
+			}
 	    }
 	}
--- OctopusUITest/Tests/OurPeopleTests.cs
+++ OctopusUITest/Tests/OurPeopleTests.cs
@@ -1,61 +1,61 @@
-﻿	using OctopusUITest.Framework;
-	using OctopusUITest.Pages;
-	using NUnit.Framework;
-	using OpenQA.Selenium.Support.PageObjects;
-	namespace OctopusUITest.Tests
-	{
-	    public class OurPeopleTests : BasePage
-	    {
-	        [Test]
-			public void TestValidPeopleSearch()
-			{
-				var ourPeoplePage = new OurPeoplePage(getDriver());
-				//Creating an instance of the page component class using PageFactory
-	            PageFactory.InitElements(getDriver(),ourPeoplePage);
+﻿    using OctopusUITest.Framework;
+    using OctopusUITest.Pages;
+    using NUnit.Framework;
+    using OpenQA.Selenium.Support.PageObjects;
+    namespace OctopusUITest.Tests
+    {
+        public class OurPeopleTests : BasePage
+        {
+            [Test]
+            public void TestValidPeopleSearch()
+            {
+                var ourPeoplePage = new OurPeoplePage(GetDriver());
+                //Creating an instance of the page component class using PageFactory
+                PageFactory.InitElements(GetDriver(),ourPeoplePage);
 
-	            //This is where the action is performed
-	            //Close Pop Up
-	            ourPeoplePage.Click(ourPeoplePage.FindByLinkText("Continue"));
-	            ourPeoplePage.InputSearch("Andy");
+                //This is where the action is performed
+                //Close Pop Up
+                ourPeoplePage.Click(ourPeoplePage.FindByLinkText("Continue"));
+                ourPeoplePage.InputSearch("Andy");
 
-	            Assert.IsTrue(ourPeoplePage.VerifyTextContains("Andy"));
+                Assert.IsTrue(ourPeoplePage.VerifyTextContains("Andy"));
 
-	        }
+            }
 
-			[Test]
-			public void TestInvalidPeopleSearch()
-			{
-				var ourPeoplePage = new OurPeoplePage(getDriver());
-				//Creating an instance of the page component class using PageFactory
-				PageFactory.InitElements(getDriver(), ourPeoplePage);
+            [Test]
+            public void TestInvalidPeopleSearch()
+            {
+                var ourPeoplePage = new OurPeoplePage(GetDriver());
+                //Creating an instance of the page component class using PageFactory
+                PageFactory.InitElements(GetDriver(), ourPeoplePage);
 
-				//This is where the action is performed
-				//Close Pop Up
-				ourPeoplePage.Click(ourPeoplePage.FindByLinkText("Continue"));
-				ourPeoplePage.InputSearch("Invalid String");
+                //This is where the action is performed
+                //Close Pop Up
+                ourPeoplePage.Click(ourPeoplePage.FindByLinkText("Continue"));
+                ourPeoplePage.InputSearch("Invalid String");
 
-				Assert.IsTrue(ourPeoplePage.VerifyTextContains("No results found"));
+                Assert.IsTrue(ourPeoplePage.VerifyTextContains("No results found"));
 
-			}
+            }
 
-			[Test]
-			public void TestNavigateToPeopleIntroPage()
-			{
-				var ourPeoplePage = new OurPeoplePage(getDriver());
-				//Creating an instance of the page component class using PageFactory
-				PageFactory.InitElements(getDriver(), ourPeoplePage);
+            [Test]
+            public void TestNavigateToPeopleIntroPage()
+            {
+                var ourPeoplePage = new OurPeoplePage(GetDriver());
+                //Creating an instance of the page component class using PageFactory
+                PageFactory.InitElements(GetDriver(), ourPeoplePage);
 
-				//This is where the action is performed
-				//Close Pop Up
-				ourPeoplePage.Click(ourPeoplePage.FindByLinkText("Continue"));
-				ourPeoplePage.InputSearch("Andy");
+                //This is where the action is performed
+                //Close Pop Up
+                ourPeoplePage.Click(ourPeoplePage.FindByLinkText("Continue"));
+                ourPeoplePage.InputSearch("Andy");
 
-				Assert.IsTrue(ourPeoplePage.VerifyTextContains("Andy"));
+                Assert.IsTrue(ourPeoplePage.VerifyTextContains("Andy"));
 
-	            ourPeoplePage.ClickResult();
+                ourPeoplePage.ClickResult();
 
-	            Assert.IsTrue(ourPeoplePage.VerifyTextContains("Andy"));
+                Assert.IsTrue(ourPeoplePage.VerifyTextContains("Andy"));
 
-			}
-	    }
-	}
+            }
+        }
+    }
